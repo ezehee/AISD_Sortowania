56ae2a8589759ad8c60cd0523cd62b3f7338663c
kmeze <kmeze@EZEHESPC>
2023-11-08 18:54:37 +0100

    Dodaj pliki projektów.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SB = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SS = new System.Windows.Forms.Button();
+            this.SI = new System.Windows.Forms.Button();
+            this.SM = new System.Windows.Forms.Button();
+            this.SQ = new System.Windows.Forms.Button();
+            this.NAPISCZAS = new System.Windows.Forms.Label();
+            this.CZAS = new System.Windows.Forms.Label();
+            this.checkBox = new System.Windows.Forms.CheckBox();
+            this.NUD = new System.Windows.Forms.NumericUpDown();
+            this.GENERATE = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.NUD)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // SB
+            // 
+            this.SB.Location = new System.Drawing.Point(100, 350);
+            this.SB.Name = "SB";
+            this.SB.Size = new System.Drawing.Size(75, 30);
+            this.SB.TabIndex = 0;
+            this.SB.Text = "SB";
+            this.SB.UseVisualStyleBackColor = true;
+            this.SB.Click += new System.EventHandler(this.SB_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(100, 100);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(175, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(100, 250);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(175, 20);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // SS
+            // 
+            this.SS.Location = new System.Drawing.Point(200, 350);
+            this.SS.Name = "SS";
+            this.SS.Size = new System.Drawing.Size(75, 30);
+            this.SS.TabIndex = 3;
+            this.SS.Text = "SS";
+            this.SS.UseVisualStyleBackColor = true;
+            this.SS.Click += new System.EventHandler(this.SS_Click);
+            // 
+            // SI
+            // 
+            this.SI.Location = new System.Drawing.Point(300, 350);
+            this.SI.Name = "SI";
+            this.SI.Size = new System.Drawing.Size(75, 30);
+            this.SI.TabIndex = 4;
+            this.SI.Text = "SI";
+            this.SI.UseVisualStyleBackColor = true;
+            this.SI.Click += new System.EventHandler(this.SI_Click);
+            // 
+            // SM
+            // 
+            this.SM.Location = new System.Drawing.Point(400, 350);
+            this.SM.Name = "SM";
+            this.SM.Size = new System.Drawing.Size(75, 30);
+            this.SM.TabIndex = 5;
+            this.SM.Text = "SM";
+            this.SM.UseVisualStyleBackColor = true;
+            this.SM.Click += new System.EventHandler(this.SM_Click);
+            // 
+            // SQ
+            // 
+            this.SQ.Location = new System.Drawing.Point(500, 350);
+            this.SQ.Name = "SQ";
+            this.SQ.Size = new System.Drawing.Size(75, 30);
+            this.SQ.TabIndex = 6;
+            this.SQ.Text = "SQ";
+            this.SQ.UseVisualStyleBackColor = true;
+            this.SQ.Click += new System.EventHandler(this.SQ_Click);
+            // 
+            // NAPISCZAS
+            // 
+            this.NAPISCZAS.AutoSize = true;
+            this.NAPISCZAS.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.NAPISCZAS.Location = new System.Drawing.Point(432, 253);
+            this.NAPISCZAS.Name = "NAPISCZAS";
+            this.NAPISCZAS.Size = new System.Drawing.Size(43, 17);
+            this.NAPISCZAS.TabIndex = 7;
+            this.NAPISCZAS.Text = "Czas:";
+            this.NAPISCZAS.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // CZAS
+            // 
+            this.CZAS.AutoSize = true;
+            this.CZAS.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.CZAS.Location = new System.Drawing.Point(497, 253);
+            this.CZAS.Name = "CZAS";
+            this.CZAS.Size = new System.Drawing.Size(68, 17);
+            this.CZAS.TabIndex = 8;
+            this.CZAS.Text = ". . . . . . . .";
+            this.CZAS.TextChanged += new System.EventHandler(this.CZAS_TextChanged);
+            // 
+            // checkBox
+            // 
+            this.checkBox.AutoSize = true;
+            this.checkBox.Location = new System.Drawing.Point(500, 103);
+            this.checkBox.Name = "checkBox";
+            this.checkBox.Size = new System.Drawing.Size(140, 17);
+            this.checkBox.TabIndex = 9;
+            this.checkBox.Text = "Nacisnij aby aktywować";
+            this.checkBox.UseVisualStyleBackColor = true;
+            this.checkBox.CheckedChanged += new System.EventHandler(this.checkBox_CheckedChanged);
+            // 
+            // NUD
+            // 
+            this.NUD.Enabled = false;
+            this.NUD.Location = new System.Drawing.Point(500, 126);
+            this.NUD.Maximum = new decimal(new int[] {
+            2000000,
+            0,
+            0,
+            0});
+            this.NUD.Name = "NUD";
+            this.NUD.Size = new System.Drawing.Size(120, 20);
+            this.NUD.TabIndex = 10;
+            this.NUD.Tag = "";
+            // 
+            // GENERATE
+            // 
+            this.GENERATE.Enabled = false;
+            this.GENERATE.Location = new System.Drawing.Point(500, 152);
+            this.GENERATE.Name = "GENERATE";
+            this.GENERATE.Size = new System.Drawing.Size(75, 23);
+            this.GENERATE.TabIndex = 11;
+            this.GENERATE.Text = "Generuj";
+            this.GENERATE.UseVisualStyleBackColor = true;
+            this.GENERATE.Click += new System.EventHandler(this.GENERATE_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.GENERATE);
+            this.Controls.Add(this.NUD);
+            this.Controls.Add(this.checkBox);
+            this.Controls.Add(this.CZAS);
+            this.Controls.Add(this.NAPISCZAS);
+            this.Controls.Add(this.SQ);
+            this.Controls.Add(this.SM);
+            this.Controls.Add(this.SI);
+            this.Controls.Add(this.SS);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.SB);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.NUD)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button SB;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button SS;
+        private System.Windows.Forms.Button SI;
+        private System.Windows.Forms.Button SM;
+        private System.Windows.Forms.Button SQ;
+        private System.Windows.Forms.Label NAPISCZAS;
+        private System.Windows.Forms.Label CZAS;
+        private System.Windows.Forms.CheckBox checkBox;
+        private System.Windows.Forms.NumericUpDown NUD;
+        private System.Windows.Forms.Button GENERATE;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,339 @@
+﻿using System;
+using System.Diagnostics;
+using System.Globalization;
+using System.Security.Cryptography;
+using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void CZAS_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void SB_Click(object sender, EventArgs e)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            if (checkBox.Checked)
+            {
+                int rnd = (int)NUD.Value;
+                string napis = NSplit(rnd);
+                int[] tab = Convert(napis);
+                tab = SortB(tab);
+                textBox2.Text = ToStr(tab);
+            }
+            else
+            {
+                int[] tab = Convert(textBox1.Text);
+                tab = SortB(tab);
+                textBox2.Text = ToStr(tab);
+            }
+            stopwatch.Stop();
+            CZAS.Text = stopwatch.Elapsed.TotalSeconds.ToString() + "s";
+        }
+        private void SS_Click(object sender, EventArgs e)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            if (checkBox.Checked)
+            {
+                int rnd = (int)NUD.Value;
+                string napis = NSplit(rnd);
+                int[] tab = Convert(napis);
+                tab = SortS(tab);
+                textBox2.Text = ToStr(tab);
+            }
+            else
+            {
+                int[] tab = Convert(textBox1.Text);
+                tab = SortS(tab);
+                textBox2.Text = ToStr(tab);
+            }
+            stopwatch.Stop();
+            CZAS.Text = stopwatch.Elapsed.TotalSeconds.ToString() + "s";
+        }
+        private void SI_Click(object sender, EventArgs e)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            if (checkBox.Checked)
+            {
+                int rnd = (int)NUD.Value;
+                string napis = NSplit(rnd);
+                int[] tab = Convert(napis);
+                tab = SortI(tab);
+                textBox2.Text = ToStr(tab);
+            }
+            else
+            {
+                int[] tab = Convert(textBox1.Text);
+                tab = SortI(tab);
+                textBox2.Text = ToStr(tab);
+            }
+            stopwatch.Stop();
+            CZAS.Text = stopwatch.Elapsed.TotalSeconds.ToString() + "s";
+        }
+        private void SM_Click(object sender, EventArgs e)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            if (checkBox.Checked)
+            {
+                int rnd = (int)NUD.Value;
+                string napis = NSplit(rnd);
+                int[] tab = Convert(napis);
+                tab = SortM(tab);
+                textBox2.Text = ToStr(tab);
+            }
+            else
+            {
+                int[] tab = Convert(textBox1.Text);
+                tab = SortM(tab);
+                textBox2.Text = ToStr(tab);
+            }
+            stopwatch.Stop();
+            CZAS.Text = stopwatch.Elapsed.TotalSeconds.ToString() + "s";
+        }
+        private void SQ_Click(object sender, EventArgs e)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            if (checkBox.Checked)
+            {
+                int rnd = (int)NUD.Value;
+                string napis = NSplit(rnd);
+                int[] tab = Convert(napis);
+                tab = SortQ(tab);
+                textBox2.Text = ToStr(tab);
+            }
+            else
+            {
+                int[] tab = Convert(textBox1.Text);
+                tab = SortQ(tab);
+                textBox2.Text = ToStr(tab);
+            }
+            stopwatch.Stop();
+            CZAS.Text = stopwatch.Elapsed.TotalSeconds.ToString() + "s";
+        }
+        int[] Convert(String napis)
+        {
+            var LiczbyS = napis.Trim().Split(' ');
+            var liczby = new int[LiczbyS.Length];
+            for (int i = 0; i < LiczbyS.Length; i++)
+            {
+                liczby[i] = int.Parse(LiczbyS[i]);
+            }
+            return liczby;
+        }
+        string NSplit(int liczba)
+        {
+            string nbstr = liczba.ToString();
+            string wynik = "";
+            foreach(char cyfra in nbstr)
+            {
+                wynik += cyfra + " ";
+            }
+            wynik = wynik.Trim();
+            return wynik;
+        }
+        String ToStr(int[] tab)
+        {
+            String re = " ";
+            for (int i = 0; i < tab.Length; i++)
+            {
+                re += tab[i] + ", ";
+            }
+            return re;
+        }
+        int[] SortB(int[] tab)
+        {
+            bool cbz;
+            do
+            {
+                cbz = false;
+                int tmp;
+                for (int i = 0; i < tab.Length - 1; i++)
+                {
+                    if (tab[i] > tab[i + 1])
+                    {
+                        tmp = tab[i];
+                        tab[i] = tab[i + 1];
+                        tab[i + 1] = tmp;
+                        cbz = true;
+                    }
+                }
+            } while (cbz);
+            return tab;
+        }
+        int[] SortS(int[] tab)
+        {
+            for (int f = 0; f < tab.Length - 1; f++)
+            {
+                int min = f;
+                for (int i = f + 1; i < tab.Length; i++)
+                {
+                    if (tab[i] < tab[min])
+                    {
+                        min = i;
+                    }
+                }
+                int temp = tab[f];
+                tab[f] = tab[min];
+                tab[min] = temp;
+            }
+            return tab;
+        }
+        int[] SortI(int[] tab)
+        {
+            for(int i = 1; i < tab.Length;i++)
+            {
+                int temp = tab[i];
+                int j = i - 1;
+                while (j >= 0 && tab[j] > temp)
+                {
+                    tab[j + 1] = tab[j];
+                    j--;
+                }
+                tab[j + 1] = temp;
+            }
+            return tab;
+        }
+        int[] SortM(int[] tab)
+        {
+            if (tab.Length <= 1) return tab;
+
+            int mid = tab.Length / 2;
+
+            int[] left = Array.Empty<int>();
+            int[] right = Array.Empty<int>();
+
+            left = new int[mid];
+            right = new int[tab.Length - mid];
+
+            left = copyArray(tab, 0, mid);
+            right = copyArray(tab, mid, tab.Length);
+
+            SortM(left);
+            SortM(right);
+
+            merge(tab, left, right);
+
+            return tab;
+        }
+        private void merge(int[] tab, int[] left, int[] right)
+        {
+            int l = 0;
+            int r = 0;
+            int i = 0;
+            while (l < left.Length && r < right.Length)
+            {
+                if (left[l] < right[r])
+                {
+                    tab[i] = left[l];
+                    l++;
+                    i++;
+                }
+                else
+                {
+                    tab[i] = right[r];
+                    r++;
+                    i++;
+                }
+            }
+            while (l < left.Length)
+            {
+                tab[i] = left[l];
+                l++;
+                i++;
+            }
+            while (r < right.Length)
+            {
+                tab[i] = right[r];
+                r++;
+                i++;
+            }
+            return;
+        }
+        private int[] copyArray(int[] src, int start, int stop)
+        {
+            int[] ret = new int[stop - start];
+            for (int i = start; i < stop; i++)
+                ret[i - start] = src[i];
+            return ret;
+        }
+        int[] SortQ(int[] tab)
+        {
+            QSR(tab, 0, tab.Length - 1);
+            return tab;
+        }
+
+        private static void QSR(int[] tab, int left, int right)
+        {
+            if (left < right)
+            {
+                int pivotIndex = Partition(tab, left, right);
+                QSR(tab, left, pivotIndex - 1);
+                QSR(tab, pivotIndex + 1, right);
+            }
+        }
+
+        private static int Partition(int[] tab, int left, int right)
+        {
+            int pivot = tab[right];
+            int i = left - 1;
+
+            for (int j = left; j < right; j++)
+            {
+                if (tab[j] < pivot)
+                {
+                    i++;
+                    Swap(tab, i, j);
+                }
+            }
+
+            Swap(tab, i + 1, right);
+            return i + 1;
+        }
+
+        private static void Swap(int[] tab, int i, int j)
+        {
+            int temp = tab[i];
+            tab[i] = tab[j];
+            tab[j] = temp;
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void checkBox_CheckedChanged(object sender, EventArgs e)
+        {
+            NUD.Enabled = checkBox.Checked;
+            GENERATE.Enabled = checkBox.Checked;
+            textBox1.Enabled = !checkBox.Checked;
+        }
+
+        private void GENERATE_Click(object sender, EventArgs e)
+        {
+            Random random = new Random();
+            int rnd = random.Next(1, (int)NUD.Maximum + 1);
+            NUD.Value = rnd;
+        }
+    }
+}
